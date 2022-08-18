6258ca284b84f8cb3d27fc3cbf754debd9826cc0
AHNDAEUN <dean034@naver.com>
2022-08-18 15:15:57 +0900

    commit 버전바꿈

---
+++ src/test/resources/com/iu/testhome/BankMemberDAO.java
@@ -0,0 +1,14 @@
+package com.iu.testhome;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BankMemberDAO {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
